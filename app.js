a38c5ab74679e9973352706f80c6c4fe97059e94
Ramiro Coll Donetz <ramirocolldonetz@Ramiros-MacBook-Pro.local>
2023-08-16 21:50:10 -0300

    add mysql db support

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const app = express();
 
-// var mysql = require('mysql');
+var mysql = require('mysql');
 
-// var connection = mysql.createConnection({
-//   host     : process.env.RDS_HOSTNAME,
-//   user     : process.env.RDS_USERNAME,
-//   password : process.env.RDS_PASSWORD,
-//   port     : process.env.RDS_PORT
-// });
+var connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+});
 
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error('Database connection failed: ' + err.stack);
-//     return;
-//   }
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
 
-//   console.log('Connected to database.');
-// });
+  console.log('Connected to database.');
+});
 
-// connection.end();
+connection.end();
 
 app.get('/', (req, res) => {
     res.send("bmw e46");
